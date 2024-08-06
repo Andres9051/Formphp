9106421378217890d46dc51c01b507337a28bebd
Andres9051 <aesh1509@gmail.com>
2024-08-06 14:59:33 -0500

    informacion.php

---
+++ informacion.php
@@ -0,0 +1,9 @@
+<?php
+$Identidicacion=$_POST["Identificacion"];
+$NombreApellidos=$_POST["NomApellido"];
+$Cargo=$_POST ["Cargo"];
+$Salario=$_POST ["Salario"];
+echo "La idetifiacion del Em´pleado es: ".Identidicion;
+echo "El cargo del empleado es:".cargo;
+echo"El salario del empleado es: ".Salario;
+?>
