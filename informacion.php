aea455689086f9762c12ba56721daf2ff67661ee
Andres9051 <aesh1509@gmail.com>
2024-08-06 15:20:22 -0500

    Update informacion.php

--- informacion.php
+++ informacion.php
@@ -4,6 +4,7 @@ $NombreApellidos=$_POST["NomApellido"];
 $Cargo=$_POST ["Cargo"];
 $Salario=$_POST ["Salario"];
 echo "La idetifiacion del Em´pleado es: ".Identidicion;
+echo "El nombre y apellido del empleado es: ".NomApellido;
 echo "El cargo del empleado es:".Cargo;
 echo"El salario del empleado es: ".Salario;
 ?>
